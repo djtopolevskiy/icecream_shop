d56ae089a541b112f26fe8645facfc38a2f2d9b7
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2024-06-17 17:27:31 +0300

    202406171730

--- admin panel/dashboard.php
+++ admin panel/dashboard.php
@@ -18,13 +18,49 @@ if(isset($_COOKIE['seller_id'])) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Blue Sky Summer - seller registeration page</title>
 <link rel="stylesheet" type="text/css" href="../css/admin_style.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+<!--------- font awesome cdn link ---------->
+<!--------- box icon cdn link ---------->
+<link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
 </head>
 
 <body>
     <div class="main-container">
         <?php include '../components/admin_header.php'; ?>
+        <section class="dashboard">
+            <div class="heading">
+                <h1>dashboard</h1>
+                <img src="../image/separator-img.png">
+            </div>
+            <div class="box-container">
+                <div class="box">
+                    <h3>welcome !</h3>
+                    <p><?= $fetch_profile['name']; ?></p>
+                    <a href="update.php" class="btn">update profile</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_massage = $conn->prepare("SELECT * FROM `message` ");
+                        $select_massage->execute();
+                        $number_of_msg = $select_massage->rowCount();
+                    ?>
+                    <h3><?= $number_of_msg; ?></h3>
+                    <p>unread message</p>
+                    <a href="admin_message.php" class="btn">see message</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_products = $conn->prepare("SELECT * FROM `products` WHERE seller_id = ? ");
+                        $select_products->execute([$seller_id]);
+                        $number_of_products = $select_products->rowCount();
+                    ?>
+                    <h3><?= $number_of_products; ?></h3>
+                    <p>products added</p>
+                    <a href="add_product.php" class="btn">add_product</a>
+                </div>
+            </div>
+        </section>
     </div>
+    
 
 
 
@@ -32,7 +68,7 @@ if(isset($_COOKIE['seller_id'])) {
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"> </script>
   
   <!-- custom js link -->
-  <script src="../js/script.js"></script>
+  <script src="../js/admin_script.js"></script>
 
   <?php include '../components/alert.php'; ?>
 
