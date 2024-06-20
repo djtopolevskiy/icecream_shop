84bb702d331563d7580234a5f7e63c98fab09e5d
djtopolevskiy <djtopolevskiy@gmail.com>
2024-06-21 00:16:49 +0300

    202406210016

--- admin panel/register.php
+++ admin panel/register.php
@@ -1,5 +1,44 @@
 <?php
 include '../components/connect.php';
+
+if(isset($_POST['submit'])){
+  $id = unique_id();
+  $name = $_POST['name'];
+  $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+  $email = $_POST['email'];
+  $email = filter_var($email, FILTER_SANITIZE_STRING);
+
+  $pass = sha1($_POST['pass']);
+  $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+  $cpass = sha1($_POST['cpass']);
+  $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+  $image = $_FILES['image']['name'];
+  $image = filter_var($image, FILTER_SANITIZE_STRING);
+  $ext = pathinfo($image, PATHINFO_EXTENSION);
+  $rename = unique_id().'.'.$ext;
+  $image_size = $_FILES['image']['size'];
+  $image_tmp_name = $_FILES['image']['tmp_name'];
+  $image_folder = '../uploaded_files/'.$rename;
+
+  $select_seller = $conn->prepare("SELECT * FROM `sellers` WHERE email = ?");
+  $select_seller -> execute([$email]);
+
+  if ($select_seller->rowCount() > 0) {
+    $warning_msg[] = 'email already exist!';
+  }else{
+    if($pass != $cpass) {
+      $warning_msg[] = 'confirm password not matched';
+    }else{
+      $insert_seller = $conn->prepare("INSERT INTO `sellers`(id, name, email, password, image) VALUES(?, ?, ?, ?, ?)");
+      $insert_seller->execute([$id, $name, $email, $cpass, $rename]);
+      move_uploaded_file($image_tmp_name, $image_folder);
+      $success_msg[] = 'new seller registreted! please login now';
+    }
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -53,7 +92,8 @@ include '../components/connect.php';
 
 
   <!-- sweetalert cdn link -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"> </script>
+  
   <!-- custom js link -->
   <script src="../js/script.js"></script>
 
